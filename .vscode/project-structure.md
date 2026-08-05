77a40778c819e2109d5cf44a3826435b28c8ab25
Abhishekh3007 <waliabhishek120@gmail.com>
2026-08-05 17:25:58 +0530

    operators,loops,conditionals

--- .vscode/project-structure.md
+++ .vscode/project-structure.md
@@ -5,7 +5,11 @@ This file maintains an up-to-date list of project files and structure.
 ## File Structure
 
 - 📁 `.vscode/`
+  - 📄 `.vscode\project-structure.md` (Markdown)
+- 📄 `lecture1.css` (CSS)
+- 📄 `lecture1.html` (HTML)
+- 📄 `lecture1.js` (JavaScript)
 - 📄 `README.md` (Markdown)
 
 ---
-Last updated: 2026-08-04T11:51:36.701Z
+Last updated: 2026-08-05T10:58:38.303Z
---
+++ lecture2.js
@@ -0,0 +1,91 @@
+//loops
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+// let i=5;
+// while(i>0){
+//     console.log("infinite loop");
+//     console.log(i);
+//     i--;
+// }
+
+// let i=5;
+// do{
+//     console.log(i);
+//     i--;
+// } while(i>0);
+// let arr=["a","b","c","d"];
+
+// // for(arrs of arr){
+// //     console.log(arrs);
+// // }   
+// obj={
+//     name:"abc",
+//     age:"24",
+//     city:"xyz"
+// }
+// for (let key in obj){
+//     // console.log(key);
+//     console.log(obj[key]);
+// }
+
+//---------------------------------------------------------
+//conditional statments
+// let age=17;
+// if(age>18){
+//     console.log("eligible for voting"); 
+// }else{
+//     console.log("not eligible for voting");
+// }
+
+
+// if(age>18){console.log("eligible for voting")}
+// else if(age>16){console.log("partially eligible")}
+// else{console.log("not eligible for voting")}
+
+// switch(new Date().getDay()){
+//     case 0:
+//         console.log("sunday");
+//         break;
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//         case 3:
+//             console.log("wednesday");
+//             break;
+//     default:
+//         console.log("not a valid day");
+//         break;
+
+// }
+//---------------------------------------------------------
+//arithmatic operators
+//  console.log(10+20);//30
+//  console.log(10-20);
+//   console.log(10*20);
+//   console.log(10/20);
+//   console.log(10%0);
+
+//asignment operators
+
+// let a=10;
+// let b=20;
+// a +=20;
+// b +=20
+
+
+// a-=10;
+// b-=10;
+// //logical operators
+
+// //!=
+// //==,===
+// //&&
+// // ||
+// if(a !=b && a>b){
+//     console.log("a is not equal to b and a is greater than b");
+// }
+
