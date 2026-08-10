e7da04f0098b1c08cecfd56e563ffa9623ce6c84
Abhishekh3007 <waliabhishek120@gmail.com>
2026-08-10 18:31:13 +0530

    async/await ...promise

--- lecture4.js
+++ lecture4.js
@@ -33,20 +33,51 @@
 // }
 // total_amount(1000)
 
-const orderFood = new Promise((resolve, reject) => {
-    const foodReady = true;
+// const orderFood = new Promise((resolve, reject) => {
+//     const foodReady = true;
 
-    setTimeout(() => {
-        if (foodReady) {
-            resolve(" Food delivered!");
-        } else {
-            reject(" Order cancelled");
-        }
-    }, 3000);
-});
+//     setTimeout(() => {
+//         if (foodReady) {
+//             resolve(" Food delivered!");
+//         } else {
+//             reject(" Order cancelled");
+//         }
+//     }, 3000);
+// });
 
-orderFood
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error));
+// orderFood
+//     .then((result) => console.log(result))
+//     .catch((error) => console.log(error));
 
     //async await promise combined example
+
+    function orderfood(){
+
+        return new Promise((resolve, reject)=>{
+            console.log("ordering food");
+            setTimeout(()=>{
+                const foodReady= true;
+             if(foodReady){
+                resolve("food is ready")
+
+             }else{
+                reject("food order failed");
+             }
+            },3000)
+        })
+
+    }
+    
+
+    async function eatFood(){
+        console.log("waiting for food");
+        try{
+            const result = await orderfood();
+        console.log(result);
+        console.log("eating food;")
+        }catch(error){
+            console.log(error)
+        }
+        
+    }
+    eatFood()
