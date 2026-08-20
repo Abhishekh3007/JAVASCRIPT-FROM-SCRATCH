34daaf14193394732a959c4505affc4f2350fa52
Abhishekh3007 <waliabhishek120@gmail.com>
2026-08-20 10:37:00 +0530

    active class link

--- React_Intern/interns/src/component/navbar.jsx
+++ React_Intern/interns/src/component/navbar.jsx
@@ -5,10 +5,10 @@ const navbar = () => {
     <div>
         <ul>
         <ul className='nav'>
-          <li><NavLink to='/'  className="nava" className={isActive => isActive ? 'active' : ''} >Home</NavLink></li>
-          <li><NavLink to='/about' className="navlink">About</NavLink></li>
-          <li><NavLink to='/contact' className="navlink">Contact</NavLink></li>
-          <li><NavLink to='/login' className="navlink">Login</NavLink></li>
+          <li><NavLink to='/'  className={isActive => isActive ? 'active' : ''} >Home</NavLink></li>
+          <li><NavLink to='/about' className={isActive => isActive ? 'active' : ''} >About</NavLink></li>
+          <li><NavLink to='/contact' className={isActive => isActive ? 'active' : ''} >Contact</NavLink></li>
+          <li><NavLink to='/login' className={isActive => isActive ? 'active' : ''} >Login</NavLink></li>
 
         </ul>
       </ul>
