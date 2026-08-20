eb55d853474242afbaa37c954c18d5745c2061bc
Abhishekh3007 <waliabhishek120@gmail.com>
2026-08-20 18:48:47 +0530

    redux-tool-kit

--- React_Intern/interns/src/App.jsx
+++ React_Intern/interns/src/App.jsx
@@ -3,46 +3,25 @@ import './App.css'
 // import  one from './assets/abc.png'
 // import  two from './assets/abd.png'
 // import  three from './assets/abb.jpg'
-import {useState,useEffect} from 'react';
+import {useSelector,useDispatch} from 'react-redux'
+import {increment,decrement,reset,incrementByAmount} from './features/counter/counterSlice'
+
 
 //parent component
 function App() {
-let vari=1;
-  //  const [first, setfirst] = useState(second)
-  const [state,setState]=useState(2);
-
-  function handbut(){
-    setState(state+1);
-    vari++;
-    console.log(vari);
-  }
-   function handbut1(){
-    setState(state-1);
-    vari--;
-  }
-  //variation1
-  useEffect(()=>{
-        alert("hello guys")
-
-        return ()=>{
-        alert("done with above code  cleaning up  ")
-        }
-
-
-  },[state,vari]);
+  const count=useSelector((state)=>state.counter.value)
+  const dispatch=useDispatch()
 
 
 
   return (
     <>
+     <button onClick={()=>dispatch(increment())}>+</button>
+     <button onClick={()=>dispatch(decrement())}>-</button>
+     <button onClick={()=>dispatch(reset())}>Reset</button>
+     <button onClick={()=>dispatch(incrementByAmount(5))}>Increment by 5</button>
 
-     {/* <Card   name={"Abhishekh"}  img={one}  desc={"Hello I am Abhishek wali , I am a software engineer and devops engineer"}/>
-     <Card   name={"Akshay"}  img={two}  desc={"Hello I am Akshay , I am a software engineer"}/> */}
-    {/* <Card   name={"Pradyumana"}  img={three}  desc={"Hello I am Pradyumana , I am a devops engineer"} data={"hello world"}/> */}
-   
-    <button onClick={handbut}>Increment</button>
-    <button onClick={handbut1}>decrement</button>
-    <h3>Count : {state}</h3>
+     <p>{count}</p>
 
 
 
